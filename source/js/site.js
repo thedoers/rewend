cb4915d9a9501cb53cc6b0b855d2354f4a063880
drmartinez <danny.martinez@neagen.it>
2013-12-24 13:19:25 +0100

    little modifies storytellers

--- source/js/site.js
+++ source/js/site.js
@@ -39,8 +39,8 @@ $("#storyteller1").hover(function(){
     $("h2.name_story_teller2").css("display","none");
     $("h2.name_story_teller3").css("display","none");
     $("h2.name_story_teller4").css("display","none");
-    $(".paragraph-story_teller h3.career").text("SCRITTRICE");
-    $(".paragraph-story_teller p.data").html('“Suggerirei di andare nel ristorante <span class="key-words">Yakup2</span> in Tunel Beyoglu perché ci va molta gente, puoi capire molto sulla vita a Istanbul.”');
+    $(".paragraph-story_teller h3.career").text("ATTRICE E TRADUTTRICE");
+    $(".paragraph-story_teller p.data").html('“Mi piace specialmente <span class="key-words">piazza Ortaköy</span>, e la zona dove finisce il Bosforo.”	');
 });
 
 $("#storyteller2").hover(function(){
@@ -48,8 +48,8 @@ $("#storyteller2").hover(function(){
     $("h2.name_story_teller2").css("display","block");
     $("h2.name_story_teller3").css("display","none");
     $("h2.name_story_teller4").css("display","none");
-    $(".paragraph-story_teller h3.career").text("CASALINGHA");
-    $(".paragraph-story_teller p.data").html('“<span class="key-words">Yakup2</span> in Tunel Beyoglu perché ci va molta gente, puoi capire molto sulla vita a Istanbul.”');
+    $(".paragraph-story_teller h3.career").text("SCRITTORE, DOCUMENTARISTA E REGISTA");
+    $(".paragraph-story_teller p.data").html('“Troverete Vefa, <span class="key-words">il più noto venditore di Boza</span> proprio imboccando la prima strada sul il lato destro dopo aver lasciato la moschea Süleymaniye dopo tre o quattro strade.”');
 });
 
 $("#storyteller3").hover(function(){
@@ -58,7 +58,7 @@ $("#storyteller3").hover(function(){
     $("h2.name_story_teller3").css("display","block");
     $("h2.name_story_teller4").css("display","none");
     $(".paragraph-story_teller h3.career").text("SCRITTRICE E GIORNALISTA");
-    $(".paragraph-story_teller p.data").html('“ nel ristorante <span class="key-words">Yakup2</span> in Tunel <span class="key-words">Beyoglu</span> perché ci va molta gente, puoi capire molto sulla vita a Istanbul.”');
+    $(".paragraph-story_teller p.data").html('“Suggerirei di andare nel ristorante <span class="key-words">Yakup2</span> in Tunel <span class="key-words">Beyoglu</span> perché ci va molta gente, puoi capire molto sulla vita a Istanbul.”');
 });
 
 $("#storyteller4").hover(function(){
@@ -66,8 +66,8 @@ $("#storyteller4").hover(function(){
     $("h2.name_story_teller2").css("display","none");
     $("h2.name_story_teller3").css("display","none");
     $("h2.name_story_teller4").css("display","block");
-    $(".paragraph-story_teller h3.career").text("FOTOGRAFO, PROFESSORE");
-    $(".paragraph-story_teller p.data").html('“ di andare nel ristorante <span class="key-words">Yakup2</span> in Tunel <span class="key-words">Beyoglu</span> perché ci va molta gente, puoi capire molto sulla vita a Istanbul.”');
+    $(".paragraph-story_teller h3.career").text("ARTISTA E SCRITTORE");
+    $(".paragraph-story_teller p.data").html('“Se dovessi accompagnare un amico straniero , passeggerei con lui in Beyoglu, poi lo accompagnerei alla Moschea Blu,<span class="key-words"> &laquo; l’altra &raquo;</span> Istanbul, la città tradizionale.”');
 });
 
 
