f36628c7ddd9321823c52f6c1ebad9b1edff1e9c
drmartinez <danny.martinez@neagen.it>
2014-01-09 15:38:24 +0100

    integrate i18n #RW-72 <state>  add work <time>

--- source/js/partials/storytellers.js
+++ source/js/partials/storytellers.js
@@ -3,9 +3,6 @@
  */
 
 
-
-
-
 $("#storyteller1").hover(function(){
     $("h2.name_story_teller1").css("display","block");
     $("h2.name_story_teller2").css("display","none");
@@ -20,8 +17,8 @@ $("#storyteller2").hover(function(){
     $("h2.name_story_teller2").css("display","block");
     $("h2.name_story_teller3").css("display","none");
     $("h2.name_story_teller4").css("display","none");
-    $(".paragraph-story_teller h3.career").text("SCRITTORE, DOCUMENTARISTA E REGISTA");
-    $(".paragraph-story_teller p.data").html('“Troverete Vefa, <span class="key-words">il più noto venditore di Boza</span> proprio imboccando la prima strada sul lato destro dopo aver lasciato la moschea Süleymaniye dopo tre o quattro strade.”');
+    $(".paragraph-story_teller h3.career").text(locale.coauthors.author2.career);
+    $(".paragraph-story_teller p.data").html(locale.coauthors.author2.data);
 });
 
 $("#storyteller3").hover(function(){
@@ -29,8 +26,8 @@ $("#storyteller3").hover(function(){
     $("h2.name_story_teller2").css("display","none");
     $("h2.name_story_teller3").css("display","block");
     $("h2.name_story_teller4").css("display","none");
-    $(".paragraph-story_teller h3.career").text("SCRITTRICE E GIORNALISTA");
-    $(".paragraph-story_teller p.data").html('“Suggerirei di andare nel ristorante <span class="key-words">Yakup2</span> in Tunel <span class="key-words">Beyoglu</span> perché ci va molta gente, puoi capire molto sulla vita a Istanbul.”');
+    $(".paragraph-story_teller h3.career").text(locale.coauthors.author3.career);
+    $(".paragraph-story_teller p.data").html(locale.coauthors.author3.data);
 });
 
 $("#storyteller4").hover(function(){
@@ -38,6 +35,6 @@ $("#storyteller4").hover(function(){
     $("h2.name_story_teller2").css("display","none");
     $("h2.name_story_teller3").css("display","none");
     $("h2.name_story_teller4").css("display","block");
-    $(".paragraph-story_teller h3.career").text("ARTISTA E SCRITTORE");
-    $(".paragraph-story_teller p.data").html('“Se dovessi accompagnare un amico straniero , passeggerei con lui in Beyoglu, poi lo accompagnerei alla Moschea Blu,<span class="key-words"> &laquo; l’altra &raquo;</span> Istanbul, la città tradizionale.”');
+    $(".paragraph-story_teller h3.career").text(locale.coauthors.author4.career);
+    $(".paragraph-story_teller p.data").html(locale.coauthors.author3.data);
 });
